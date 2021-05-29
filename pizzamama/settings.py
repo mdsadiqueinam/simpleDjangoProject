70c28f1ea637177aaf315f8e7a0bd4f5ad38d81d
Md Sadique Inam <32585888+mdsadiqueinam@users.noreply.github.com>
2021-05-29 10:28:27 +0530

    Please enter your own django secret key

--- pizzamama/settings.py
+++ pizzamama/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^+$obpghjkli5hgwp0@fffff(1$uo71hcf65!ezff2r1x)byn@v'
+SECRET_KEY = 'django-insecure-please_enter_your_own_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
